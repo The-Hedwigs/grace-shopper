634f91114b5fd4abfd2228d8e700a1d435850d1e
Jacqueline Zheng <jzheng318@gmail.com>
2019-09-18 11:47:45 -0400

    initial order model frame

---
+++ server/db/models/order.js
@@ -0,0 +1,29 @@
+const db = require('../db')
+const Sequelize = require('sequelize')
+
+const Order = db.define('order', {
+  //inform if order is in cart or submitted
+  submitted: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: 0
+  },
+  total: {
+    type: Sequelize.DECIMAL(100, 2)
+  },
+  shipping: {
+    type: Sequelize.TEXT
+  },
+  billing: {
+    type: Sequelize.TEXT
+  },
+  payment: {
+    type: Sequelize.ENUM('Cash', 'Credit', 'Paypal')
+  },
+  email: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Order
+
+//hooks
