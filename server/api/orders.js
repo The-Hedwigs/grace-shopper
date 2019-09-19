ab16f2fffb6d5d16b3d864867339409e1fe50f8c
Julia Catalano <jcatalano1990@gmail.com>
2019-09-19 16:18:06 -0400

    fix: deleted old code

--- server/api/orders.js
+++ server/api/orders.js
@@ -39,21 +39,4 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-router.put('/:id', async (req, res, next) => {
-  //check to see if entry exists. if it does, just update that table
-  //if entry does not exist, then addTomato
-  try {
-    const id = Number(req.params.id)
-    const order = await Order.findById(id)
-    const newTomato = await Tomatoes.findById(/* this will take in the tomato id thats submitted when we click on the add button */)
-    const tomatoPrice = newTomato.price
-    await order.addTomato(newTomato, {quantity: 1})
-    await order.update({
-      total: order.total + tomatoPrice
-    })
-    res.json(order)
-  } catch (err) {
-    next(err)
-  }
-})
 module.exports = router
