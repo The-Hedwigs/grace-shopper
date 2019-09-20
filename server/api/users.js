e5cdcdf7dde8ee2cf6df95b472b0ebd3a144813d
Pelin Dogan <pelinsezgin@gmail.com>
2019-09-19 20:13:23 -0400

    if nonadmin user tries to go to api they are redirected to the home page

--- server/api/users.js
+++ server/api/users.js
@@ -2,32 +2,47 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
+function requireAdminStatus(req, res, callback) {
+  if (req.user.isAdmin) {
+    callback()
+  } else {
+    res.redirect('/home')
   }
+}
+
+router.get('/', async (req, res, next) => {
+  requireAdminStatus(req, res, async () => {
+    try {
+      const users = await User.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ['id', 'email']
+      })
+      res.json(users)
+    } catch (err) {
+      next(err)
+    }
+  })
 })
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: ['id', 'email', 'shippingAddress', 'billingAddress']
-    })
-    res.json(user)
-  } catch (err) {
-    next(err)
-  }
+router.get('/:id', (req, res, next) => {
+  //console.log("getting user with id");
+  //console.log(req.user);
+  requireAdminStatus(req, res, async () => {
+    //I created a function checks that user`s admin situation, acoordingly returns the data or redirects unauthorized.
+    try {
+      const user = await User.findOne({
+        where: {
+          id: req.params.id
+        },
+        attributes: ['id', 'email', 'shippingAddress', 'billingAddress']
+      })
+      res.json(user)
+    } catch (err) {
+      next(err)
+    }
+  })
 })
 
 router.put('/:id', async (req, res, next) => {
