603f5396d0b55113ac8df2878c7021b4972a8b3d
Pelin Dogan <pelinsezgin@gmail.com>
2019-09-24 00:34:51 -0400

    all the links and redirections are working

--- client/components/topNavbar.js
+++ client/components/topNavbar.js
@@ -5,39 +5,58 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const TopNavbar = ({handleClick, isLoggedIn}) => (
-  <nav className="navbar navbar-expand-md navbar-dark bg-dark" style={{height: 40}}>
+  <nav
+    className="navbar navbar-expand-md navbar-dark bg-dark mt-0 mb-0 pt-0 pb-0"
+    style={{height: 40}}
+  >
     <h6 className="nav-item text-light ">
       Free shipping if you have the spicy boy in your cart!
     </h6>
-    <div className="collapse navbar-collapse justify-content-end " id="navbarNav">
-      <ul className="navbar-nav ">
+    <div
+      className="collapse navbar-collapse justify-content-end h-100"
+      id="navbarNav"
+    >
+      <ul className="navbar-nav h-100">
         {isLoggedIn ? (
-          <li className="nav-item ">
-            <Link className="nav-link" to="/accountSettings">
+          <li className="nav-item h-100">
+            <Link
+              className="nav-link h-100 mt-0 mb-0 pt-0 pb-0"
+              to="/accountSettings"
+            >
               {' '}
               My Account{' '}
             </Link>
           </li>
         ) : null}
         {isLoggedIn ? (
-          <li className="nav-item">
-            <a className="nav-link text-muted" href="#" onClick={handleClick}>
+          <li className="nav-item h-100">
+            <a
+              className="text-muted h-100 mt-0 mb-0 pt-0 pb-0"
+              href="#"
+              onClick={() => handleClick()}
+            >
               Logout
             </a>
           </li>
         ) : null}
         {!isLoggedIn ? (
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <Link className="nav-link text-muted" to="/login">
-                Login
-              </Link>
-            </a>
-            <a className="nav-link" href="#">
-              <Link className="nav-link text-muted" to="/signup">
-                Sign Up
-              </Link>
-            </a>
+          <li className="nav-item h-100">
+            <Link
+              className="nav-link text-muted h-100 mt-0 mb-0 pt-0 pb-0"
+              to="/login"
+            >
+              Login
+            </Link>
+          </li>
+        ) : null}
+        {!isLoggedIn ? (
+          <li className="nav-item h-100">
+            <Link
+              className="nav-link text-muted h-100 mt-0 mb-0 pt-0 pb-0"
+              to="/signup"
+            >
+              Sign Up
+            </Link>
           </li>
         ) : null}
       </ul>
