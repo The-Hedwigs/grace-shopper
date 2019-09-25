84abb6cbc31033d4cad8bc0a3f4d65468bfacdaf
Julia Catalano <jcatalano1990@gmail.com>
2019-09-24 23:43:41 -0400

    feature: toasts - cheers!

--- client/components/allTomatoes.js
+++ client/components/allTomatoes.js
@@ -1,27 +1,67 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import {getAllTomatoesThunk, addToCartThunk} from '../store/tomato'
-import Tomatoes from './tomatoes'
+import {ToastsContainer, ToastsStore} from 'react-toasts'
 //import {Link} from 'react-router-dom'
 
 export class AllTomatoes extends React.Component {
   constructor(props) {
     super(props)
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
     this.props.getAllTomatoes()
   }
 
+  handleClick(id) {
+    console.log('this is the id', id)
+    this.props.addToCart(id)
+    ToastsStore.success('Tomato added to cart! Yum!')
+  }
+
   render() {
     return (
       <div className="container">
+        <ToastsContainer className="toasts" store={ToastsStore} />
         <h1 className="display-4 text-center">Our Tomatoes</h1>
         {this.props.tomatoes ? (
-          <Tomatoes
-            tomatoes={this.props.tomatoes}
-            addToCart={id => this.props.addToCart(id)}
-          />
+          <div className="container">
+            <div className="row">
+              {this.props.tomatoes.map(tomato => (
+                <div className="col-md-4" key={tomato.id}>
+                  <div className="card text-center">
+                    <div className="wrapper">
+                      <img
+                        src={tomato.imageUrl}
+                        className="card-img-top img-fluid"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <Link
+                        className="card-link font-weight-bold text-danger"
+                        to={`/tomatoes/${tomato.id}`}
+                      >
+                        {tomato.name}
+                      </Link>
+                      <div className="card-subtitle mb-2 text-muted">
+                        {tomato.description}
+                      </div>
+                      <div className="card-text">{tomato.price}</div>
+                      <button
+                        type="button"
+                        className="btn btn-outline-dark btn-block"
+                        onClick={() => this.handleClick(tomato.id)}
+                      >
+                        Add 🍅 to Cart
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         ) : (
           'No tomatoes here!'
         )}
