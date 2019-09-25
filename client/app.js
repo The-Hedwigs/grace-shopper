10d6e9fc7fc54cb596b396daf4aa2fdf707add83
Jacqueline Zheng <jzheng318@gmail.com>
2019-09-25 01:12:07 -0400

    updated css for login screen

--- client/app.js
+++ client/app.js
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <TopNavbar />
       <Routes />
     </div>
   )
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,13 +6,80 @@ import {logout} from '../store'
 import {setOrderThunk} from '../store/cart'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <nav className="navbar sticky-top navbar-light bg-light" style={{height: 70}}>
-    <div className="container justify-content-center">
-      <h1 className="text-center text-uppercase font-weight-bold">
-        T🍅-ma-t🍅 Or Not T🍎-ma-t🍎?
-      </h1>
-    </div>
-  </nav>
+  <div>
+    <nav
+      className="navbar sticky-top navbar-light bg-light"
+      style={{height: 70}}
+    >
+      <div className="container justify-content-center">
+        <h1 className="text-center text-uppercase font-weight-bold">
+          T🍅-ma-t🍅 Or Not T🍎-ma-t🍎?
+        </h1>
+      </div>
+    </nav>
+    <nav
+      className="navbar navbar-expand-md navbar-dark bg-dark"
+      style={{height: 50}}
+    >
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="mx-auto order-0 w-auto">
+          <ul className="navbar-nav order-md-0">
+            <li className="nav-item active ">
+              <Link className="nav-link  text-success " to="/tomatoes">
+                🍅
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <ul className="navbar-nav justify-content-end">
+          {isLoggedIn ? (
+            <li className="nav-item">
+              <a
+                className="text-white margin-auto pt-0 pb-0 align-self-center"
+                href="#"
+                onClick={() => handleClick()}
+              >
+                Logout
+              </a>
+            </li>
+          ) : null}
+          {!isLoggedIn ? (
+            <li className="nav-item">
+              <Link
+                className="nav-link text-white margin-auto pt-0 pb-0 align-self-center"
+                to="/login"
+              >
+                Login
+              </Link>
+            </li>
+          ) : null}
+          {!isLoggedIn ? (
+            <li className="nav-item">
+              <Link
+                className="nav-link text-white margin-auto pt-0 pb-0 align-self-center"
+                to="/signup"
+              >
+                Sign Up
+              </Link>
+            </li>
+          ) : null}
+        </ul>
+        <div className="navbar-collapse collapse w-10 order-3 dual-collapse2">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item ">
+              <Link
+                className="nav-link text-white margin-auto pt-0 pb-0 align-self-center"
+                to="/cart"
+              >
+                {' '}
+                🛒{' '}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
 )
 
 /**
